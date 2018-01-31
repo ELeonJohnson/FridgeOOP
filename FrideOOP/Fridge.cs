27dce40bc718f6e2ad31cc3a3083977db1fd7678
ELeonJohnson <enochjohnsonleon@gmail.com>
2018-01-31 14:44:15 -0500

    Added Construtors And First Method

--- FrideOOP/Fridge.cs
+++ FrideOOP/Fridge.cs
@@ -41,7 +41,43 @@ namespace FrideOOP
         }
 
         //Constructors
+        public Fridge()
+        {
+            this.dispenserType = "basic water dispenser";   
+        }
+
+        public Fridge(string dispenserType)
+        {
+            this.dispenserType = dispenserType;
+        }
+
+        public Fridge(bool isClean, string dispenserType, int foodAmount, bool bulbsWork)
+        {
+            this.isClean = isClean;
+            this.dispenserType = dispenserType;
+            this.foodAmount = foodAmount;
+            this.bulbsWork = bulbsWork;
+            
+        }
 
         //Methods()
+        
+        public void DispenseWater()
+        {
+            if (dispenserType == "basic water dispenser")
+            {
+                Console.WriteLine("Here's some lukewarm water!");
+            } else if (dispenserType == "deluxe dispenser")
+            {
+                Console.WriteLine("Enjoy your perfect ice water!");
+            }
+            else
+            {
+                Console.WriteLine("What's gonna come out? It's a surprise!");
+            }
+
+
+        }
+       
     }
 }
